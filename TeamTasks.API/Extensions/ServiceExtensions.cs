94c90490ad284fd9927cb891f5d3463de6f3be6f
Jonathan Aldana <jonathanaldanatorres@gmail.com>
2026-03-10 20:49:36 -0500

    feat: se configura servicio de extensiónm base se adicionan nuggets para referenciación del dbcontext y referencia a infrastructure

---
+++ TeamTasks.API/Extensions/ServiceExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TeamTasks.Infrastructure.Persistence;
+
+namespace TeamTasks.API.Extensions
+{
+    static class ServiceExtensions
+    {
+        public static IServiceCollection AddDatabase(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+            return services;
+        }
+
+        public static IServiceCollection AddCorsPolicy(this IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("DevPolicy", policy =>
+                {
+                    policy.WithOrigins("http://localhost:4200")
+                          .AllowAnyHeader()
+                          .AllowAnyMethod();
+                });
+            });
+
+            return services;
+        }
+    }
+}
