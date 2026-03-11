fd09fc572e0d19baee9fea57d156a8c59dc60d61
Jonathan Aldana <jonathanaldanatorres@gmail.com>
2026-03-10 20:41:21 -0500

    feat: se configura appdbcontext con mapeo de tablas para bd

---
+++ TeamTasks.Infrastructure/Persistence/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TeamTasks.Domain.Entities;
+
+namespace TeamTasks.Infrastructure.Persistence
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Developer> Developers => Set<Developer>();
+        public DbSet<Task> Tasks => Set<Task>();
+        public DbSet<TaskItem> TaskItems => Set<TaskItem>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Developer>().ToTable("developers");
+            modelBuilder.Entity<Project>().ToTable("projects");
+            modelBuilder.Entity<TaskItem>().ToTable("tasks");
+        }
+    }
+}
