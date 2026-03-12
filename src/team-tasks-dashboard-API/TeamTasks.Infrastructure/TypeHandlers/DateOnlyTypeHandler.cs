ea1cb70facd5e9cc98182de7a0e4e9c1be66aaeb
Jonathan Aldana <jonathanaldanatorres@gmail.com>
2026-03-11 19:38:29 -0500

    feature: se crea handler para mapear Datetime de sql a DateOnly de c#

---
+++ src/team-tasks-dashboard-API/TeamTasks.Infrastructure/TypeHandlers/DateOnlyTypeHandler.cs
@@ -0,0 +1,19 @@
+﻿using Dapper;
+using System.Data;
+
+namespace TeamTasks.Infrastructure.TypeHandlers
+{
+    public class DateOnlyTypeHandler : SqlMapper.TypeHandler<DateOnly>
+    {
+        public override void SetValue(IDbDataParameter parameter, DateOnly value)
+        {
+            parameter.DbType = DbType.Date;
+            parameter.Value = value.ToDateTime(TimeOnly.MinValue);
+        }
+
+        public override DateOnly Parse(object value)
+        {
+            return DateOnly.FromDateTime((DateTime)value);
+        }
+    }
+}
